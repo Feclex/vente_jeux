705ef30f28ecce674292ed055bfbefddef23769d
ctilmant <tilmantcorentin@gmail.com>
2018-08-21 20:55:22 +0200

    creer user

--- src/be/atc/servlets/Register.java
+++ src/be/atc/servlets/Register.java
@@ -20,121 +20,128 @@ import be.atc.dao.EMF;
 import be.atc.dao.EntityFinderImpl;
 import be.atc.modeldb.Localite;
 import be.atc.modeldb.User;
+import be.atc.forms.CreationUserForm;
 
 
-	@WebServlet("/register")
-	public class Register extends HttpServlet{
+@WebServlet("/register")
+public class Register extends HttpServlet{
 
-		public static final String CHAMP_EMAIL = "email";
-	    public static final String CHAMP_PASS = "motdepasse";
-	    public static final String CHAMP_CONF = "confirmation";
-	    public static final String CHAMP_NOM = "login";
-	    public static final String ATT_ERREURS  = "erreurs";
-	    public static final String ATT_RESULTAT = "resultat";
+	public static final String CHAMP_EMAIL = "email";
+	public static final String CHAMP_PASS = "motdepasse";
+	public static final String CHAMP_CONF = "confirmation";
+	public static final String CHAMP_NOM = "login";
+	public static final String ATT_ERREURS  = "erreurs";
+	public static final String ATT_RESULTAT = "resultat";
+	public static final String ATT_USER = "user";
+	public static final String ATT_FORM = "creationuserform";
 	// Log4j
 	private static final Logger log = Logger.getLogger(Register.class);
-    public static final String VUE = "/Register.jsp";
-	
-    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
-        /* Affichage de la page d'inscription */
-    	EntityManager em=EMF.getEM();
-    	  
-  	  try {	
+	public static final String VUE = "/Register.jsp";
 
-  	    	List<Localite> localite=  em.createNamedQuery("Localite.findAll",Localite.class).getResultList();
-  	    	
-  	    	if (localite.isEmpty())
-  	    	log.debug("vide");
-  	    	else
-  	    	{
-  	    		request.setAttribute("localites", localite);
-  	    		this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
-  	          
-  	    	}
-  	    } catch(Exception e)
-  	    {
-  	    	log.debug("EXCEPTION BATARD");
-  	    }
-  	     finally {
-  	    	 em.close(); 
-  	    	 
-  	     }
-        
-    }
-    
-    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
-    	 /* Récupération des champs du formulaire. */
-    	  String resultat;
-          Map<String, String> erreurs = new HashMap<String, String>();
-        String email = request.getParameter( CHAMP_EMAIL );
-        String motDePasse = request.getParameter( CHAMP_PASS );
-        String confirmation = request.getParameter( CHAMP_CONF );
-        String nom = request.getParameter( CHAMP_NOM );
-        
-        /* Validation du champ email. */
+	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+		/* Affichage de la page d'inscription */
+		EntityManager em=EMF.getEM();
 
+		try {	
 
-        try {
-        	Validation.validationEmail( email );
-        } catch ( Exception e ) {
-            erreurs.put( CHAMP_EMAIL, e.getMessage() );
-        }
+			List<Localite> localite=  em.createNamedQuery("Localite.findAll",Localite.class).getResultList();
 
-        /* Validation des champs mot de passe et confirmation. */
-        try {
-        	Validation.validationMotsDePasse( motDePasse, confirmation );
-        } catch ( Exception e ) {
-            erreurs.put( CHAMP_PASS, e.getMessage() );
-        }
+			if (localite.isEmpty())
+				log.debug("vide");
+			else
+			{
+				request.setAttribute("localites", localite);
+				this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
 
-        /* Validation du champ nom. */
-        try {
-        	Validation.validationNom( nom );
-        } catch ( Exception e ) {
-            erreurs.put( CHAMP_NOM, e.getMessage() );
-        }
+			}
+		} catch(Exception e)
+		{
+			log.debug("EXCEPTION BATARD");
+		}
+		finally {
+			em.close(); 
 
-        /* Initialisation du résultat global de la validation. */
-        if ( erreurs.isEmpty() ) {
-            resultat = "Succès de l'inscription.";
-        } else {
-            resultat = "Échec de l'inscription.";
-        }
+		}
 
-        /* Stockage du résultat et des messages d'erreur dans l'objet request */
-        request.setAttribute( ATT_ERREURS, erreurs );
-        request.setAttribute( ATT_RESULTAT, resultat );
-        
-        EntityManager em=EMF.getEM();
-  	  
-    	  try {	
+	}
 
-    	    	List<Localite> localite=  em.createNamedQuery("Localite.findAll",Localite.class).getResultList();
-    	    	
-    	    	if (localite.isEmpty())
-    	    	log.debug("vide");
-    	    	else
-    	    	{
-    	    		request.setAttribute("localites", localite);
-    	    	}
-    	    } catch(Exception e)
-    	    {
-    	    	log.debug("EXCEPTION BATARD");
-    	    }
-    	     finally {
-    	    	 em.close(); 
-    	    	 
-    	     }
-
-        /* Transmission de la paire d'objets request/response à notre JSP */
-        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
-    }
-
-    
+	public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+		/* Récupération des champs du formulaire. */
+		String resultat;
+		Map<String, String> erreurs = new HashMap<String, String>();
+		String email = request.getParameter( CHAMP_EMAIL );
+		String motDePasse = request.getParameter( CHAMP_PASS );
+		String confirmation = request.getParameter( CHAMP_CONF );
+		String nom = request.getParameter( CHAMP_NOM );
 
 
 
-    
-	
+		/* Validation du champ email. */
+
+
+		try {
+			Validation.validationEmail( email );
+		} catch ( Exception e ) {
+			erreurs.put( CHAMP_EMAIL, e.getMessage() );
+		}
+
+		/* Validation des champs mot de passe et confirmation. */
+		try {
+			Validation.validationMotsDePasse( motDePasse, confirmation );
+		} catch ( Exception e ) {
+			erreurs.put( CHAMP_PASS, e.getMessage() );
+		}
+
+		/* Validation du champ nom. */
+		try {
+			Validation.validationNom( nom );
+		} catch ( Exception e ) {
+			erreurs.put( CHAMP_NOM, e.getMessage() );
+		}
+
+		/* Initialisation du résultat global de la validation. */
+		if ( erreurs.isEmpty() ) {
+			resultat = "Succès de l'inscription.";
+		} else {
+			resultat = "Échec de l'inscription.";
+		}
+
+		/* Stockage du résultat et des messages d'erreur dans l'objet request */
+		request.setAttribute( ATT_ERREURS, erreurs );
+		request.setAttribute( ATT_RESULTAT, resultat );
+
+		EntityManager em=EMF.getEM();
+
+		try {	
+
+			List<Localite> localite=  em.createNamedQuery("Localite.findAll",Localite.class).getResultList();
+
+			if (localite.isEmpty())
+				log.debug("vide");
+			else
+			{
+				request.setAttribute("localites", localite);
+			}
+		} catch(Exception e)
+		{
+			log.debug("EXCEPTION BATARD");
+		}
+		finally {
+			em.close(); 
+
+		}
+
+		/* Appel au traitement et à la validation de la requête, et récupération du bean en résultant */
+		CreationUserForm newuser = new CreationUserForm();
+		User user = newuser.creerUser(request);
+
+		/* Stockage du formulaire et du bean dans l'objet request */
+		request.setAttribute( ATT_FORM, newuser );
+		request.setAttribute( ATT_USER, user );
+
+		/* Transmission de la paire d'objets request/response à notre JSP */
+		this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+	}
+
 
 }
